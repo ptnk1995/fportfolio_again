ae9702fadcbac81c84ed24bcd0c6ce271629a689
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-04-01 18:36:10 +0700

    refactor-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout
+
+  def layout
+    is_a?(Devise::SessionsController) ? false : "application"
+  end
 end
