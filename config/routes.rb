021c2c1f1afeba9dfcfec99792fd77848c536a26
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-04-02 11:01:30 +0700

    ui contact

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
   resources :projects
-  resources :blogs
+  resources :blogs, :contacts
   resources :projects do
     resources :teams
   end
