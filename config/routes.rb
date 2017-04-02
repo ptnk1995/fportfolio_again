8a51eaeb3339e20067992cae10371f73ced07a40
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-04-02 09:13:57 +0700

    ui list show project

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
   resources :projects
+  resources :tops
   namespace :admin do
     root "static_pages#home"
   end
