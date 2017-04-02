f33543827e68eedf7aa9f14cf96ba6928d9a447a
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-04-02 11:44:16 +0700

    add link to

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
   resources :projects
-  resources :blogs, :contacts
+  resources :blogs, :contacts, :abouts
   resources :projects do
     resources :teams
   end
