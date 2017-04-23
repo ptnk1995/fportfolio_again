259bf69f60cbe863f32767079e304c8395e9dd6e
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-04-23 21:59:56 +0700

    fix-contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
-class ContactsController < BaseController
+class ContactsController < ApplicationController
+  layout "application"
   def index
     @contact = Contact.new
   end
@@ -8,7 +9,7 @@ class ContactsController < BaseController
     if @contact.save
       flash[:success] = t ".contact_success"
       ContactMailer.user_send_contact(@contact).deliver_now
-      binding.pry
+      #binding.pry
       redirect_to root_url
     else
       flash[:danger] = t ".contact_error"
